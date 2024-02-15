298c17bb37453472dbc5dbaa61f9497268ab4c1a
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-02-15 17:31:42 -0500

    easy

--- app/app.js
+++ app/app.js
@@ -3,6 +3,11 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'embroider-with-webpack-build-plugins-demo/config/environment';
 
+import * as info from '~build/package';
+import * as meta from '~build/meta';
+
+console.log({ info, meta });
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,10 +2,12 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function (defaults) {
+module.exports = async function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
   });
+
+  const { default: info } = await import('unplugin-info/webpack');
 
   const { Webpack } = require('@embroider/webpack');
   return require('@embroider/compat').compatBuild(app, Webpack, {
@@ -20,5 +22,19 @@ module.exports = function (defaults) {
         package: 'qunit',
       },
     ],
+    packagerOptions: {
+      webpackConfig: {
+        plugins: [
+          info({
+            meta: async () => {
+              return {
+                message: 'Hello world!, from info.meta!',
+              };
+            },
+            package: { dependencies: true, devDependencies: true },
+          }),
+        ],
+      },
+    },
   });
 };
